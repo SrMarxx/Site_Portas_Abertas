f37b4ba0357053659c6087288d775434d14aa0e7
Sr <marcosgabrieldiascardoso66@gmail.com>
2023-11-12 00:56:07 -0300

    Criação do app cadastro

--- portas_abertas/portas_abertas/settings.py
+++ portas_abertas/portas_abertas/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'cadastro.apps.CadastroConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'portas_abertas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'portas_abertas',
+        'USER': 'root',
+        'PASSWORD': 'Mgdc8514!',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
@@ -103,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
