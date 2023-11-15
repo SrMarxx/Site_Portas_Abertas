fee6ceee2e13ec4f172eeceb41b41667b27a241b
Sr <marcosgabrieldiascardoso66@gmail.com>
2023-11-14 21:12:59 -0300

    Criando modelos no banco de dados

--- portas_abertas/cadastro/models.py
+++ portas_abertas/cadastro/models.py
@@ -1,3 +1,10 @@
-from django.db import models
+from django.db import models, migrations
 
 # Create your models here.
+
+class User(models.Model):
+    id = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
+    nome = models.CharField(max_length=255, default='')
+    email = models.CharField(max_length=255, default='')
+    telefone = models.CharField(max_length=15, default='')
+    setor = models.CharField(max_length=40, default='')
